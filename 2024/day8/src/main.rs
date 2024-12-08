c2c418bfabc6363f753996e61774866cc7fc6dfb
Maxime Poncet <maxime.poncet@gmail.com>
2024-12-08 20:02:13 +0100

    day 8 decluttered

--- 2024/day8/src/main.rs
+++ 2024/day8/src/main.rs
@@ -36,38 +36,17 @@ fn part1(input: &str) -> usize {
     let Game { grid, antennas } = parse(input);
 
     let mut antinodes = HashSet::new();
-    for antenna in antennas.values() {
-        let antennas_combinations = antenna.iter().combinations(2);
+    for positions in antennas.values() {
+        let permutations = positions.iter().permutations(2);
 
-        for combination in antennas_combinations {
-            let (antenna1, antenna2) = (combination[0], combination[1]);
-            let diff_row = antenna2.0.abs_diff(antenna1.0) as i32;
-            let diff_col = antenna2.1.abs_diff(antenna1.1) as i32;
+        for permutation in permutations {
+            let (antenna1, antenna2) = (permutation[0], permutation[1]);
+            let diff_row = antenna2.0 - antenna1.0;
+            let diff_col = antenna2.1 - antenna1.1;
 
-            let mut antinode1 = (0, 0);
-            let mut antinode2 = (0, 0);
-            if antenna1.0 < antenna2.0 {
-                antinode1.0 = antenna1.0 - diff_row;
-                antinode2.0 = antenna2.0 + diff_row;
-            } else {
-                antinode1.0 = antenna1.0 + diff_row;
-                antinode2.0 = antenna2.0 - diff_row;
-            }
-
-            if antenna1.1 < antenna2.1 {
-                antinode1.1 = antenna1.1 - diff_col;
-                antinode2.1 = antenna2.1 + diff_col;
-            } else {
-                antinode1.1 = antenna1.1 + diff_col;
-                antinode2.1 = antenna2.1 - diff_col;
-            }
-
-            if grid.get(antinode1.0, antinode1.1).is_some() {
-                antinodes.insert(antinode1);
-            }
-
-            if grid.get(antinode2.0, antinode2.1).is_some() {
-                antinodes.insert(antinode2);
+            let antinode = (antenna2.0 + diff_row, antenna2.1 + diff_col);
+            if grid.get(antinode.0, antinode.1).is_some() {
+                antinodes.insert(antinode);
             }
         }
     }
@@ -79,41 +58,13 @@ fn part2(input: &str) -> usize {
     let Game { grid, antennas } = parse(input);
 
     let mut antinodes = HashSet::new();
-    for antenna in antennas.values() {
-        let antennas_combinations = antenna.iter().combinations(2);
+    for positions in antennas.values() {
+        let permutations = positions.iter().permutations(2);
 
-        for combination in antennas_combinations {
-            let (antenna1, antenna2) = (combination[0], combination[1]);
-            let diff_row = antenna2.0.abs_diff(antenna1.0) as i32;
-            let diff_col = antenna2.1.abs_diff(antenna1.1) as i32;
-
-            let mut vec1 = (0, 0);
-            let mut vec2 = (0, 0);
-            if antenna1.0 < antenna2.0 {
-                vec1.0 = -diff_row;
-                vec2.0 = diff_row;
-            } else {
-                vec1.0 = diff_row;
-                vec2.0 = -diff_row;
-            }
-
-            if antenna1.1 < antenna2.1 {
-                vec1.1 = -diff_col;
-                vec2.1 = diff_col;
-            } else {
-                vec1.1 = diff_col;
-                vec2.1 = -diff_col;
-            }
-
-            let mut antinode = *antenna1;
-            loop {
-                if grid.get(antinode.0, antinode.1).is_some() {
-                    antinodes.insert(antinode);
-                } else {
-                    break;
-                }
-                antinode = (antinode.0 + vec1.0, antinode.1 + vec1.1);
-            }
+        for permutation in permutations {
+            let (antenna1, antenna2) = (permutation[0], permutation[1]);
+            let diff_row = antenna2.0 - antenna1.0;
+            let diff_col = antenna2.1 - antenna1.1;
 
             let mut antinode = *antenna2;
             loop {
@@ -122,7 +73,7 @@ fn part2(input: &str) -> usize {
                 } else {
                     break;
                 }
-                antinode = (antinode.0 + vec2.0, antinode.1 + vec2.1);
+                antinode = (antinode.0 + diff_row, antinode.1 + diff_col);
             }
         }
     }
